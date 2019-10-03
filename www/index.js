636c744485bd04d98ef67b3c7899a12c7de656d2
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-10-03 13:00:48 +0200

    Added a button for future save possibilities

--- www/index.js
+++ www/index.js
@@ -11,17 +11,26 @@ let div = document.createElement('div')
 div.setAttribute('class', 'input-holder')
 body.append(div)
 
+// Input to insert name
 let nameInput = document.createElement('input')
 nameInput.setAttribute('class', 'insert-name')
 nameInput.setAttribute('placeholder', 'Ange namn:')
 div.append(nameInput)
 
+// Input for mail
 let mailInput = document.createElement('input')
 mailInput.setAttribute('class', 'insert-mail')
 mailInput.setAttribute('placeholder', 'Ange mail:')
 div.append(mailInput)
 
+// input for telephone number
 let telephoneInput = document.createElement('input')
 telephoneInput.setAttribute('class', 'insert-number')
 telephoneInput.setAttribute('placeholder', 'Ange telefon nr:')
 div.append(telephoneInput)
+
+// Button to save the contacts insertet valuse
+let submit = document.createElement('button')
+submit.setAttribute('class', 'save-data')
+submit.innerHTML = 'Lägg till'
+div.append(submit)
