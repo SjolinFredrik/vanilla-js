ddaa2516b8fbfcf35c0f49505401e347f87c95f6
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-10-03 11:08:23 +0200

    Change div to be an header

--- www/index.js
+++ www/index.js
@@ -1,5 +1,6 @@
+// Body element for the whole site
 let body = document.querySelector('body')
 
-let div = document.createElement('div')
-div.innerHTML = 'Hello, I love Phoney!'
-body.append(div)
+let header = document.createElement('header')
+header.innerHTML = 'Hello, I love Phoney!'
+body.append(header)
