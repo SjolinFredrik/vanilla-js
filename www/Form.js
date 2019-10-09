d563911f1022faca2479908e659b5c9f8e288ac2
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-10-09 15:53:44 +0200

    Adding more divs to get a good layout for the inputs/buttons in form class

--- www/Form.js
+++ www/Form.js
@@ -24,48 +24,59 @@ class Form extends App {
     div.append(nameInput)
 
     // Input for mail
+    const emailDiv = document.createElement('div')
+    emailDiv.setAttribute('class', 'email-div')
     let mailInput = document.createElement('input')
     mailInput.setAttribute('type', 'text')
     mailInput.setAttribute('class', 'add-mail')
     mailInput.setAttribute('value', '')
     mailInput.setAttribute('placeholder', 'Ange mail:')
     mailInput.setAttribute('id', 'email')
-    div.append(mailInput)
-
-    // input for telephone number
-    let telephoneInput = document.createElement('input')
-    telephoneInput.setAttribute('type', 'text')
-    telephoneInput.setAttribute('class', 'add-number')
-    telephoneInput.setAttribute('value', '')
-    telephoneInput.setAttribute('placeholder', 'Ange telefon nr:')
-    telephoneInput.setAttribute('id', 'telephone')
-    div.append(telephoneInput)
+    emailDiv.append(mailInput)
+    div.append(emailDiv)
 
     // button to add more emails
     let addEmail = document.createElement('button')
     addEmail.setAttribute('class', 'add-more')
     addEmail.setAttribute('id', 'add-more-email')
     addEmail.innerHTML = '+'
-    div.append(addEmail)
+    emailDiv.append(addEmail)
+
+    // input for telephone number
+    const phoneDiv = document.createElement('div')
+    phoneDiv.setAttribute('class', 'phone-div')
+    let telephoneInput = document.createElement('input')
+    telephoneInput.setAttribute('type', 'text')
+    telephoneInput.setAttribute('class', 'add-number')
+    telephoneInput.setAttribute('value', '')
+    telephoneInput.setAttribute('placeholder', 'Ange telefon nr:')
+    telephoneInput.setAttribute('id', 'telephone')
+    phoneDiv.append(telephoneInput)
+    div.append(phoneDiv)
 
     // button to add more telephone number
     let addTelephoneNumber = document.createElement('button')
     addTelephoneNumber.setAttribute('class', 'add-more')
-    addTelephoneNumber.setAttribute('id', 'add-telephone-number')
+    addTelephoneNumber.setAttribute('id', 'add-more-phone')
     addTelephoneNumber.innerHTML = '+'
-    div.append(addTelephoneNumber)
+    phoneDiv.append(addTelephoneNumber)
 
-    // Button to save the contacts insertet valuse
+    // button to save the contacts insertet value
+    let buttonDiv = document.createElement('div')
+    buttonDiv.setAttribute('class', 'button-div')
     let submit = document.createElement('button')
     submit.setAttribute('class', 'saved-user')
     submit.setAttribute('id', 'save-contact')
     submit.innerHTML = 'Lägg till mig!'
-    div.append(submit)
+    buttonDiv.append(submit)
+    div.append(buttonDiv)
 
     // Button to remove contact from phonebook
+
     let deleteButton = document.createElement('button')
     deleteButton.setAttribute('class', 'remove-user')
     deleteButton.innerHTML = 'Radera mig!'
-    div.append(deleteButton)
+    buttonDiv.append(deleteButton)
+    div.append(buttonDiv)
   }
 }
