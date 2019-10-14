2a4b4cf42ac5f7ae852156de557089062f6d8d2a
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-10-14 15:25:04 +0200

    fixed things for edit and delete button in contacts

--- www/Contacts.js
+++ www/Contacts.js
@@ -3,6 +3,7 @@ class Contacts extends App {
     super()
     this.addedContacts()
   }
+
   addedContacts() {
     let tableDiv = document.createElement('div')
     tableDiv.setAttribute('class', 'table-div')
@@ -12,16 +13,23 @@ class Contacts extends App {
     let table = document.createElement('table')
     let thead = document.createElement('thead')
     let tr = document.createElement('tr')
+
     let th = document.createElement('th')
     th.innerText = 'Namn'
+
     let thTwo = document.createElement('th')
     thTwo.innerText = 'E-post'
+
     let thThree = document.createElement('th')
     thThree.innerText = 'Telefon'
+
+    let thFour = document.createElement('th')
+    thFour.innerText = 'Editera'
 
     tr.append(th)
     tr.append(thTwo)
     tr.append(thThree)
+    tr.append(thFour)
     thead.append(tr)
     table.append(thead)
 
@@ -32,16 +40,33 @@ class Contacts extends App {
       let td = document.createElement('td')
       td.innerText = contact.name
 
-      let tdTwo = document.createElement('tdTwo')
-      tdTwo.innerText = contact.email
+      let tdTwo = document.createElement('td')
+      tdTwo.innerText = contact.email.join('\n')
 
-      let tdThree = document.createElement('tdThree')
-      tdThree.innerText = contact.telephone
+      let tdThree = document.createElement('td')
+      tdThree.innerText = contact.phone.join('\n')
+
+      let tdFour = document.createElement('td')
+      let editButton = document.createElement('button')
+      editButton.setAttribute('class', 'edit-contact')
+      editButton.setAttribute('data', contact.id)
+      editButton.innerText = 'Editera'
+      thFour.innerText = 'Editera'
+
+      let tdFive = document.createElement('td')
+      let deleteButton = document.createElement('button')
+      deleteButton.setAttribute('class', 'delete-contact')
+      deleteButton.setAttribute('data', contact.id)
+      deleteButton.innerText = 'Radera'
 
       tr.append(td)
-      tr.append(tdTwo)
       tr.append(tdThree)
+      tr.append(tdTwo)
+      tr.append(tdFour)
+      tr.append(tdFive)
       tbody.append(tr)
+      tdFour.append(editButton)
+      tdFive.append(deleteButton)
     })
     table.append(tbody)
     tableDiv.append(table)
