f1350e00528caf93a2897421b9c46c5df0df42d9
Fredrik Sjolin <sjolin.fredrik@gmail.com>
2019-10-17 10:45:42 +0200

    added contactHistory / Created latestVersion
    latestVersion is a copy of contact.history so that we don't change the original array when we want to edit our contact. latestVersion is the person that's always the "active" on. 
    Created a table for the history and a forEach loop to go through the history array with their given index. on the resetButton I put in data-index so we know which place the contact have in the array.

--- www/Contact.js
+++ www/Contact.js
@@ -1,24 +1,21 @@
 class Contact extends App {
-  constructor() {
+  constructor(id) {
     super()
+    this.id = id
+    this.addedContact()
+    this.contactHistory()
   }
-
   // get a specific id for each contact from contacts list
-  addedContact(id) {
+  addedContact() {
     let contact = contacts.find(contact => {
-      return contact.id === Number(id)
+      return contact.id === Number(this.id)
     })
+    const latestVersion = [...contact.history].pop(-1)
     console.log(contact)
-
-    // Re-create the form for each contact
-    // let div = document.createElement('div')
-    // div.setAttribute('class', 'form-div')
-    // let body = document.querySelector('body')
-    // body.append(div)
 
     // The excisting contact information
     let tableDiv = document.createElement('div')
-    tableDiv.setAttribute('class', 'table-div')
+    tableDiv.setAttribute('class', 'table-div contact')
     let body = document.querySelector('body')
     body.append(tableDiv)
 
@@ -36,7 +33,7 @@ class Contact extends App {
     thThree.innerText = 'Telefon'
 
     let thFour = document.createElement('th')
-    thFour.innerText = 'Uppdatera'
+    thFour.innerText = ''
 
     tr.append(th)
     tr.append(thTwo)
@@ -51,24 +48,92 @@ class Contact extends App {
     let tr2 = document.createElement('tr')
 
     let td = document.createElement('td')
-    td.innerText = contact.name
+    td.innerText = latestVersion.name
+    console.log(contact.history)
 
     let tdTwo = document.createElement('td')
-    tdTwo.innerText = contact.email
+    tdTwo.innerText = latestVersion.email.join('\n')
 
     let tdThree = document.createElement('td')
-    tdThree.innerText = contact.phone
+    tdThree.innerText = latestVersion.phone.join('\n')
 
     let updateButton = document.createElement('button')
     updateButton.setAttribute('class', 'update-button')
     updateButton.setAttribute('data', contact.id)
-    updateButton.innerText = 'Uppdatera'
+    updateButton.innerText = 'Editera'
 
     tr2.append(td)
-    tr2.append(tdThree)
     tr2.append(tdTwo)
+    tr2.append(tdThree)
     tr2.append(updateButton)
     tbody.append(tr2)
+    table.append(tbody)
+  }
+  contactHistory() {
+    let contact = contacts.find(contact => {
+      return contact.id === Number(this.id)
+    })
+    console.log(contact)
+
+    // The excisting contact information
+    let tableDiv = document.createElement('div')
+    tableDiv.setAttribute('class', 'table-div history')
+    let body = document.querySelector('body')
+    body.append(tableDiv)
+
+    let table = document.createElement('table')
+    let thead = document.createElement('thead')
+    let tr = document.createElement('tr')
+
+    let th = document.createElement('th')
+    th.innerText = 'Namn'
+
+    let thTwo = document.createElement('th')
+    thTwo.innerText = 'E-post'
+
+    let thThree = document.createElement('th')
+    thThree.innerText = 'Telefon'
+
+    let thFour = document.createElement('th')
+
+    tr.append(th)
+    tr.append(thTwo)
+    tr.append(thThree)
+    tr.append(thFour)
+
+    thead.append(tr)
+    table.append(thead)
+    tableDiv.append(table)
+
+    let tbody = document.createElement('tbody')
+    contact.history.forEach((contactVersion, i) => {
+      let tr2 = document.createElement('tr')
+
+      let td = document.createElement('td')
+      td.innerText = contactVersion.name
+
+      let tdTwo = document.createElement('td')
+      tdTwo.innerText = contactVersion.email.join('\n')
+
+      let tdThree = document.createElement('td')
+      tdThree.innerText = contactVersion.phone.join('\n')
+
+      let tdFour = document.createElement('td')
+
+      let resetButton = document.createElement('button')
+      resetButton.setAttribute('class', 'reset-button')
+      resetButton.setAttribute('data-index', i)
+      resetButton.setAttribute('data-id', this.id)
+
+      resetButton.innerText = 'Återställ'
+      tdFour.append(resetButton)
+
+      tr2.append(td)
+      tr2.append(tdTwo)
+      tr2.append(tdThree)
+      tr2.append(tdFour)
+      tbody.append(tr2)
+    })
     table.append(tbody)
   }
 }
